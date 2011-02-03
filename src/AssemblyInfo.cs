c488733f90b21cdb200881f001c459c7eea38f64
Gleb Golubitsky <sectoid@gnolltech.org>
2011-02-04 00:40:35 +0200

    Creating repository for haskell drafts
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

---
+++ src/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+// -*- mode: csharp; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+
+[assembly: System.Reflection.AssemblyTitle("Language.Haskell.Internal")]
+[assembly: System.Reflection.AssemblyDescription("Internal stuff of haskell drafts")]
+[assembly: System.Reflection.AssemblyCompany("Gnolltech")]
+[assembly: System.Reflection.AssemblyCopyright("LGPL 3.0")]
---
+++ src/th/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+// -*- mode: csharp; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+
+[assembly: System.Reflection.AssemblyTitle("Language.Haskell.TH.Syntax")]
+[assembly: System.Reflection.AssemblyDescription("Language.Haskell.TH.Syntax module")]
+[assembly: System.Reflection.AssemblyCompany("Gnolltech")]
+[assembly: System.Reflection.AssemblyCopyright("LGPL 3.0")]
