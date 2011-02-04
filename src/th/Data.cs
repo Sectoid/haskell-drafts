28026f3de408ecf7935362a137f0974e403eed48
Gleb Golubitsky <sectoid@gnolltech.org>
2011-02-04 17:04:39 +0200

    Some new types were added to Language.Haskell.TH.Syntax module
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- src/th/Data.cs
+++ src/th/Data.cs
@@ -1,3 +1,5 @@
+// -*- mode: csharp; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+
 using Prelude;
 using Language.Haskell.Runtime;
 
@@ -8,17 +10,13 @@ namespace Language.Haskell.TH.Syntax
 // data Name = Name OccName NameFlavour deriving (Typeable, Data)
 [ADT]
 [ADTCtor]
-public class Name 
+public class Name
 {
-    public OccName OccName;
-    public NameFlavour NameFlavour;
+    public System.Tuple<OccName, NameFlavour> Items;
 
-    public Name(OccName occName,
-                NameFlavour nf)
-    {
-      OccName = occName;
-      NameFlavour = nf;
-    }
+    public Name(OccName v1,
+                NameFlavour v2)
+    { Items = System.Tuple.Create(v1, v2); }
 }
 
 // data NameFlavour
@@ -31,84 +29,75 @@ public class NameS : NameFlavour {}
 
 //   | NameQ ModName   -- ^ A qualified name; dynamically bound
 [ADTCtor]
-public class NameQ : NameFlavour 
-{ 
-    public ModName ModName;
-    public NameQ(ModName modName) { ModName = modName; }
+public class NameQ : NameFlavour
+{
+    public System.Tuple<ModName> Items;
+    public NameQ(ModName v1) { Items = System.Tuple.Create(v1); }
 }
 
 //   | NameU Int#      -- ^ A unique local name
 [ADTCtor]
-public class NameU : NameFlavour 
-{ 
-    public int Id;
-    public NameU(int id) { Id = id; }
+public class NameU : NameFlavour
+{
+    public System.Tuple<int> Items;
+    public NameU(int v1) { Items = System.Tuple.Create(v1); }
 }
 
 
 //   | NameL Int#      -- ^ Local name bound outside of the TH AST
 [ADTCtor]
-public class NameL : NameFlavour 
-{ 
-    public int Id;
-    public NameL(int id) { Id = id; }
+public class NameL : NameFlavour
+{
+    public System.Tuple<int> Items;
+    public NameL(int v1) { Items = System.Tuple.Create(v1); }
 }
 
 //   | NameG NameSpace PkgName ModName -- ^ Global name bound outside of the TH AST:
 [ADTCtor]
-public class NameG : NameFlavour 
+public class NameG : NameFlavour
 {
-    public NameSpace NameSpace;
-    public PkgName PkgName;
-    public ModName ModName;
-
-    public NameG(NameSpace ns,
-                 PkgName pkg,
-                 ModName mod)
+    public System.Tuple<NameSpace, PkgName, ModName> Items;
+    public NameG(NameSpace v1, PkgName v2, ModName v3)
     {
-      NameSpace = ns;
-      PkgName = pkg;
-      ModName = mod;
+      Items = System.Tuple.Create(v1, v2, v3);
     }
 }
 
 // newtype ModName = ModName String	-- Module name
 [ADT]
 [ADTCtor]
-public class ModName 
+public class ModName
 {
-    public String String;
-    public ModName(String name)
+    public System.Tuple<String> Items;
+    public ModName(String v1)
     {
-      String = name;
+      Items = System.Tuple.Create(v1);
     }
 }
 
 // newtype PkgName = PkgName String	-- package name
 [ADT]
 [ADTCtor]
-public class PkgName 
+public class PkgName
 {
-    public String String;
-    public PkgName(String name)
+    public System.Tuple<String> Items;
+    public PkgName(String v1)
     {
-      String = name;
+      Items = System.Tuple.Create(v1);
     }
 }
 
 // newtype OccName = OccName String
 [ADT]
 [ADTCtor]
-public class OccName 
+public class OccName
 {
-    public String String;
-    public OccName(String name)
+    public System.Tuple<String> Items;
+    public OccName(String v1)
     {
-      String = name;
+      Items = System.Tuple.Create(v1);
     }
 }
-
-
 
 // data NameSpace
 [ADT]
@@ -118,149 +107,116 @@ public abstract class NameSpace {}
 [ADTCtor]
 public class VarName : NameSpace {}
 
-// 	       | DataName	-- ^ Data constructors 
+//             | DataName	-- ^ Data constructors
 [ADTCtor]
 public class DataName : NameSpace {}
 
-// 	       | TcClsName	-- ^ Type constructors and classes; Haskell has them
-// 				-- in the same name space for now.
+//             | TcClsName	-- ^ Type constructors and classes; Haskell has them
+//                              -- in the same name space for now.
 [ADTCtor]
 public class TcClsName : NameSpace {}
 
-// // data Dec
-// [ADT]
-// public abstract class Dec {}
+// data Dec
+[ADT]
+public abstract class Dec {}
 
-// // FunD Name [Clause]  -- { f p1 p2 = b where decs }
-// [ADTCtor]
-// public class FunD : Dec
-// {
-//     public Name Name;
-//     public List<Clause> Clauses;
-//     public FunD(Name name, List<Clause> clauses)
-//     {
-//       Name = name;
-//       Clauses = clauses;
-//     }
-// }
+// FunD Name [Clause]  -- { f p1 p2 = b where decs }
+[ADTCtor]
+public class FunD : Dec
+{
+    public System.Tuple<Name, List<Clause>> Items;
+    public FunD(Name v1, List<Clause> v2)
+    { Items = System.Tuple.Create(v1, v2); }
+}
 
-// // ValD Pat Body [Dec]	{ p = b where decs }
-// [ADTCtor]
-// public class ValD : Dec
-// {
-//     public Pat Pat;
-//     public Body Body;
-//     public List<Dec> Decs;
-    
-//     public ValD(Pat pat,
-// 		Body body,
-// 		List<Dec> decs)
-//     {
-//       Pat = pat;
-//       Body = body;
-//       Decs = decs;
-//     }
-// }
+// ValD Pat Body [Dec]	{ p = b where decs }
+[ADTCtor]
+public class ValD : Dec
+{
+    public System.Tuple<Pat, Body, List<Dec>> Items;
 
-// // DataD Cxt Name [TyVarBndr] [Con] [Name]     { data Cxt x => T x = A x | B (T x)  deriving (Z,W)}
-// [ADTCtor]
-// public class DataD : Dec
-// {
-//     public Cxt Ctx;
-//     public Name Name;
-//     public List<TyVarBndr> TyVarBndrs;
-//     public List<Con> Cons;
-//     public List<Name> Names;
+    public ValD(Pat v1, Body v2, List<Dec> v3)
+    { Items = System.Tuple.Create(v1, v2, v3); }
+}
 
-//     public DataD(Cxt ctx,
-// 		 Name name,
-// 		 List<TyVarBndr> tyVarBndrs,
-// 		 List<Con> cons,
-// 		 List<Name> names)
-//     {
-//       Ctx = ctx;
-//       Name = name;
-//       TyVarBndrs = tyVarBndrs;
-//       Cons = cons;
-//       Names = names;
-//     }
-// }
+// DataD Cxt Name [TyVarBndr] [Con] [Name]     { data Cxt x => T x = A x | B (T x)  deriving (Z,W)}
+[ADTCtor]
+public class DataD : Dec
+{
+    public System.Tuple<Ctx, Name, List<TyVarBndr>,
+                        List<Con>, List<Name>> Items;
 
-// // NewtypeD Cxt Name [TyVarBndr] Con [Name]	{ newtype Cxt x => T x = A (B x)       deriving (Z,W)}
-// [ADTCtor]
-// public class NewtypeD : Dec
-// {
-//     public Cxt Ctx;
-//     public Name Name;
-//     public List<TyVarBndr> TyVarBndrs;
-//     public Con Con;
-//     public List<Name> Names;
+    public DataD(Ctx v1, Name v2, List<TyVarBndr> v3,
+                 List<Con> v4, List<Name> v5)
+    { Items = System.Tuple.Create(v1, v2, v3, v4, v5); }
+}
 
-//     public NewtypeD(Cxt ctx,
-// 		    Name name,
-// 		    List<TyVarBndr> tyVarBndrs,
-// 		    Con con,
-// 		    List<Name> names)
-//     {
-//       Ctx = ctx;
-//       Name = name;
-//       TyVarBndrs = tyVarBndrs;
-//       Con = con;
-//       Names = names;
-//     }
-// }
+// NewtypeD Cxt Name [TyVarBndr] Con [Name]	{ newtype Cxt x => T x = A (B x)       deriving (Z,W)}
+[ADTCtor]
+public class NewtypeD : Dec
+{
+    public System.Tuple<Ctx, Name, List<TyVarBndr>,
+                        Con, List<Name>> Items;
 
-// // TySynD Name [TyVarBndr] Type	{ type T x = (x,x) }
-// [ADTCtor]
-// public class TySynD : Dec
-// {
-//     public Name Name;
-//     public List<TyVarBndr> TyVarBndrs;
-//     public Type Type;
+    public NewtypeD(Ctx v1, Name v2, List<TyVarBndr> v3,
+                    Con v4, List<Name> v5)
+    { Items = System.Tuple.Create(v1, v2, v3, v4, v5); }
+}
 
-//     public TySynD(Name name,
-// 		  List<TyVarBndr> tyVarBndrs,
-// 		  Type type)
-//     {
-//       Name = name;
-//       TyVarBndrs = tyVarBndrs;
-//       Type = type;
-//     }
-// }
+// TySynD Name [TyVarBndr] Type	{ type T x = (x,x) }
+[ADTCtor]
+public class TySynD : Dec
+{
+    public System.Tuple<Name, List<TyVarBndr>, Type> Items;
 
-// // ClassD Cxt Name [TyVarBndr] [FunDep] [Dec]	{ class Eq a => Ord a where ds }
-// [ADTCtor]
-// public class ClassD : Dec
-// {
-//     public Ctx Ctx;
-//     public Name Name;
-//     public List<TyVarBndr> TyVarBndrs;
-//     public List<FunDep> FunDeps;
-//     public List<Dec> Decs;
+    public TySynD(Name v1, List<TyVarBndr> v2, Type v3)
+    { Items = System.Tuple.Create(v1, v2, v3); }
+}
 
-//     public ClassD(Ctx ctx,
-// 		  Name name,
-// 		  List<TyVarBndr> tyVarBndrs,
-// 		  List<FunDep> funDeps,
-// 		  List<Dec> decs)
-//     {
-//       Ctx = ctx;
-//       Name = name;
-//       TyVarBndrs = tyVarBndrs;
-//       FunDeps = funDeps;
-//       Decs = decs;
-//     }
-// }
+// ClassD Cxt Name [TyVarBndr] [FunDep] [Dec]	{ class Eq a => Ord a where ds }
+[ADTCtor]
+public class ClassD : Dec
+{
+    public System.Tuple<Ctx, Name, List<TyVarBndr>, 
+                        List<FunDep>, List<Dec>> Items;
 
-// // // InstanceD Cxt Type [Dec]	{ instance Show w => Show [w]       where ds }
-// // [ADTCtor]
-// // public class InstanceD
+    public ClassD(Ctx v1, Name v2, List<TyVarBndr> v3,
+                  List<FunDep> v4, List<Dec> v5)
+    { Items = System.Tuple.Create(v1, v2, v3, v4, v5); }
+}
 
-
-
+// // InstanceD Cxt Type [Dec]	{ instance Show w => Show [w]       where ds }
+[ADTCtor]
+public class InstanceD : Dec
+{
+    public System.Tuple<Ctx, Type, List<Dec>> Items;
+    public InstanceD(Ctx v1, Type v2, List<Dec> v3)
+    { Items = System.Tuple.Create(v1, v2, v3); }
+}
 
 // TODO
 [ADT]
 public abstract class Clause {}
+
+[ADT]
+public abstract class Pat {}
+
+[ADT]
+public abstract class Body {}
+
+[ADT]
+public abstract class Ctx {}
+
+[ADT]
+public abstract class Con {}
+
+[ADT]
+public abstract class TyVarBndr {}
+
+[ADT]
+public abstract class Type {}
+
+[ADT]
+public abstract class FunDep {}
 
 }
