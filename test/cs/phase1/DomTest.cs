b2df495464e298ba6dd5ec65090feeb5de6aff18
Gleb Golubitsky <sectoid@gnolltech.org>
2011-02-18 16:26:23 +0200

    [Language.Haskell.Phase1.Tests] Dom smoke test
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- test/cs/phase1/DomTest.cs
+++ test/cs/phase1/DomTest.cs
@@ -1,2 +1,32 @@
 // -*- mode: csharp; indent-tabs-mode: nil; c-basic-offset: 2; -*-
 
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace Language.Haskell.Phase1.DOM
+{
+
+[TestFixture]
+public class DomTests
+{
+
+  [Test]
+  public void SillyDeserialization()
+  {
+    using(var reader = new StringReader(TestData.XmlAST))
+    {
+      var loader = new XmlSerializer(typeof(HsModule));
+      var module = loader.Deserialize(reader);
+
+      Assert.That(module, Is.Not.Null);
+      Assert.That(module, Is.TypeOf(typeof(HsModule)));
+    }
+  }
+
+}
+
+}
