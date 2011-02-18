1f764cea50680943f02f3c867b0f01580ce5d3cf
Gleb Golubitsky <sectoid@gnolltech.org>
2011-02-18 14:37:13 +0200

    [Language.Haskell.Phase1] Created a stub assembly
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

---
+++ src/cs/phase1/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+// -*- mode: csharp; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+
+[assembly: System.Reflection.AssemblyTitle("Language.Haskell.Phase1")]
+[assembly: System.Reflection.AssemblyDescription("Haskell Drafts Phase1 assembly")]
+[assembly: System.Reflection.AssemblyCompany("Gnolltech")]
+[assembly: System.Reflection.AssemblyCopyright("LGPL 3.0")]
---
+++ src/cs/phase1/Dom.cs
@@ -0,0 +1,9 @@
+// -*- mode: csharp; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+
+using System;
+
+namespace Language.Haskell.Phase1.DOM
+{
+
+
+}
