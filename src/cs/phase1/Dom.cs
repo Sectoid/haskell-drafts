b35958a69df8b16bea75701643b47885b052b2fc
Gleb Golubitsky <sectoid@gnolltech.org>
2011-02-22 16:29:44 +0200

    [Language.Haskell.Phase1.Tests] First silly visitor support for Dom
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- src/cs/phase1/Dom.cs
+++ src/cs/phase1/Dom.cs
@@ -8,36 +8,69 @@ using System.Xml.Serialization;
 namespace Language.Haskell.Phase1.DOM
 {
 
-public class SrcLoc
+public interface IAstNode
+{
+  IAstNode Parent { get; set; }
+  void accept(IVisitor visitor);
+}
+
+public abstract class BasicNode : IAstNode
+{
+  [XmlIgnore]
+  public IAstNode Parent { get; set; }
+  public abstract void accept(IVisitor visitor);
+}
+
+public class SrcLoc : BasicNode
 {
   [XmlAttribute("srcLine")]
   public int Line { get; set; }
   [XmlAttribute("srcColumn")]
-  public int Column {get; set; }
+  public int Column { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
-public class HsName
+public class HsName : BasicNode
 {
   [XmlText]
   public string Name { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
-public abstract class HsQName {}
+public abstract class HsQName : BasicNode {}
 
 public class UnQual : HsQName
 {
   [XmlText]
   public string Name { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
-public class HsModule
+public class HsModule : BasicNode
 {
   public SrcLoc Location { get; set; }
   public string Module { get; set; }
   public List<HsDecl> Body { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
-public abstract class HsDecl 
+public abstract class HsDecl : BasicNode
 {
   public SrcLoc Location { get; set; }
 }
@@ -46,94 +79,170 @@ public class HsTypeSig : HsDecl
 {
   public List<HsName> Names { get; set; }
   public HsQualType Type { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
-public class HsQualType
+public class HsQualType : BasicNode
 {
   public HsContext Context { get; set; }
   public HsType Type { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
-public class HsContext {}
+public class HsContext : BasicNode 
+{
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
+}
 
-public abstract class HsType {}
+public abstract class HsType : BasicNode {}
 
 public class HsTyApp : HsType
 {
   public HsType First { get; set; }
   public HsType Second { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
-public class HsTyCon
+public class HsTyCon : BasicNode
 {
   public HsQName Name { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
 public class HsPatBind : HsDecl
 {
   public HsPat Pattern { get; set; }
   public HsRhs Rhs { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
-public abstract class HsPat {}
+public abstract class HsPat : BasicNode {}
 
 public class HsPVar : HsPat
 {
   public HsName Name { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
-public abstract class HsRhs {}
+public abstract class HsRhs : BasicNode {}
 
 public class HsUnGuardedRhs : HsRhs
 {
   public HsExp Expression { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
-public abstract class HsExp {}
+public abstract class HsExp : BasicNode {}
 
 public class HsInfixApp : HsExp
 {
   public HsExp Left { get; set; }
   public HsQOp Op { get; set; }
   public HsExp Right { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
 public class HsApp : HsExp
 {
   public HsExp Left { get; set; }
   public HsExp Right { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
 public class HsVar : HsExp
 {
   public HsQName Name { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
 public class HsRightSection : HsExp
 {
   public HsQOp Op { get; set; }
   public HsExp Right { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
 public class HsLit : HsExp
 {
   public HsLiteral Value { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
 public class HsEnumFromTo : HsExp
 {
   public HsExp From { get; set; }
   public HsExp To { get; set; }
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
-public abstract class HsLiteral {}
+public abstract class HsLiteral : BasicNode {}
 
 public abstract class HsInt : HsLiteral
 {
   [XmlText]
   public int Value;
+
+  public override void accept(IVisitor visitor)
+  {
+    visitor.visit(this);
+  }
 }
 
-public abstract class HsQOp {}
+public abstract class HsQOp : BasicNode {}
 
 }
---
+++ src/cs/phase1/IVisitor.cs
@@ -0,0 +1,32 @@
+// -*- mode: csharp; indent-tabs-mode: nil; c-basic-offset: 2; -*-
+
+using System;
+using Language.Haskell.Phase1.DOM;
+
+namespace Language.Haskell.Phase1
+{
+
+public interface IVisitor
+{
+  void visit(SrcLoc node);
+  void visit(HsName node);
+  void visit(UnQual node);
+  void visit(HsModule node);
+  void visit(HsTypeSig node);
+  void visit(HsQualType node);
+  void visit(HsContext node);
+  void visit(HsPatBind node);
+  void visit(HsTyApp node);
+  void visit(HsTyCon node);
+  void visit(HsPVar node);
+  void visit(HsUnGuardedRhs node);
+  void visit(HsInfixApp node);
+  void visit(HsApp node);
+  void visit(HsVar node);
+  void visit(HsRightSection node);
+  void visit(HsLit node);
+  void visit(HsEnumFromTo node);
+  void visit(HsInt node);
+}
+
+}
